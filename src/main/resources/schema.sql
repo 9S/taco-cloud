c8bca360ec46cc1681e88a50756b2e80938117e9
Michael <michael@springer.dev>
2021-09-20 08:58:33 +0200

    Convert Database to POSTGRESQL

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,28 +6,44 @@ drop table if exists Ingredient;
 
 create table if not exists Ingredient
 (
-    id   varchar(4)  not null,
+    id   varchar(4)  PRIMARY KEY,
     name varchar(25) not null,
     type varchar(10) not null
 );
 create table if not exists Taco
 (
-    id        identity,
+    id        bigserial PRIMARY KEY,
     name      varchar(50) not null,
     createdAt timestamp   not null
 );
 create table if not exists Taco_Ingredients
 (
-    taco_id        bigint     not null,
-    ingredients_id varchar(4) not null
+    taco_id        bigint,
+    ingredients_id varchar(4)
 );
 alter table Taco_Ingredients
+    add primary key (taco_id, ingredients_id),
     add foreign key (taco_id) references Taco (id);
+
 alter table Taco_Ingredients
     add foreign key (ingredients_id) references Ingredient (id);
+
+CREATE TABLE if not exists "user"
+(
+    id           BIGSERIAL PRIMARY KEY ,
+    username     VARCHAR(255) NULL,
+    password     VARCHAR(255) NULL,
+    fullname     VARCHAR(255) NULL,
+    street       VARCHAR(255) NULL,
+    city         VARCHAR(255) NULL,
+    state        VARCHAR(255) NULL,
+    zip          VARCHAR(255) NULL,
+    phone_number VARCHAR(255) NULL
+);
+
 create table if not exists Taco_Order
 (
-    id             identity,
+    id             bigserial PRIMARY KEY ,
     deliveryName   varchar(50) not null,
     deliveryStreet varchar(50) not null,
     deliveryCity   varchar(50) not null,
@@ -36,28 +52,18 @@ create table if not exists Taco_Order
     ccNumber       varchar(16) not null,
     ccExpiration   varchar(5)  not null,
     ccCVV          varchar(3)  not null,
-    placedAt       timestamp   not null
+    placedAt       timestamp   not null,
+    user_id        bigint
 );
+alter table Taco_Order
+    add foreign key (user_id) references "user" (id);
+
 create table if not exists Taco_Order_Tacos
 (
-    order_id bigint not null,
-    tacos_id bigint not null
+    order_id bigint,
+    tacos_id bigint
 );
 alter table Taco_Order_Tacos
-    add foreign key (order_id) references Taco_Order (id);
-alter table Taco_Order_Tacos
+    add primary key (order_id, tacos_id),
+    add foreign key (order_id) references Taco_Order (id),
     add foreign key (tacos_id) references Taco (id);
-
-CREATE TABLE if not exists user
-(
-    id           BIGINT       NOT NULL,
-    username     VARCHAR(255) NULL,
-    password     VARCHAR(255) NULL,
-    fullname     VARCHAR(255) NULL,
-    street       VARCHAR(255) NULL,
-    city         VARCHAR(255) NULL,
-    state        VARCHAR(255) NULL,
-    zip          VARCHAR(255) NULL,
-    phone_number VARCHAR(255) NULL,
-    CONSTRAINT pk_user PRIMARY KEY (id)
-);
